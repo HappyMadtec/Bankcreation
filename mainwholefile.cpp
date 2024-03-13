f42c9793be1de9e144da370cd1640c001f21e947
HappyMadtec <happymadtec@hotmail.com>
2024-03-13 21:19:58 +0100

    new commit

--- mainwholefile.cpp
+++ mainwholefile.cpp
@@ -1,12 +1,99 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <fstream>
 
+// use design patterns in the creation of this bank simulation 
+// do unit tests -> check for libraries
+// check for technics to verify the tests (memory leaks -- read rational test realtime)
+//{memory leak , performance bottleneck, logic error in c++ code}
 using namespace std;
+class PrintingFiles {
+public:
+	int balance;
+    PrintingFiles(const string& name, const string& account, double initialBalance) {}
 
-class UserDataBase { 
-    public: 
-        char name[10];
+    void printingFilesTXT(ofstream& outputFile);
+    int printingUser2Files(const string& username);
+};
+
+void PrintingFiles::printingFilesTXT(ofstream& outputFile) {
+    outputFile << "|------------------------------------" << endl;
+    outputFile << "|          Bank Statement for  << name << " << endl;
+    outputFile << "|          Account:  << user_account << " << endl;
+    outputFile << "|------------------------------------" << endl;
+    outputFile << endl;
+    outputFile << endl;
+    outputFile << "Your balance is: " << balance << endl;
+    outputFile << endl;
+}
+
+int PrintingFiles::printingUser2Files(const string& username) {
+    string filename = username + ".txt";
+
+    ifstream fileCheck(filename);
+    if (fileCheck.good()) {
+        cout << "File exists" << endl;
+    }
+    fileCheck.close();
+
+    ofstream outputFile(filename, ios::out | ios::trunc);
+    if (!outputFile.is_open()) {
+        cerr << "Error opening file" << endl;
+        return 1;
+    } else {
+        printingFilesTXT(outputFile);
+        return 0;
+    }
+}
+
+class MyBankActions  { //composition
+private: 
+	UserDataBase operations;
+public:
+	void deposit();
+	void withdraw();
+	void displayStatement();
+	void transfer();
+
+
+};
+
+void MyBankActions::deposit(){
+	int depositValue = 0;
+	
+	cout<<"You chose to deposit!"<<endl;
+	
+	cin>>depositValue;
+	
+	operations.balance += depositValue;
+
+	cout<<"Dear customer your new balance is: "<<operations.balance<<endl;
+}
+
+void MyBankActions::withdraw(){
+	int withdrawValue = 0;
+	cout<<"You chose to withdraw!"<<endl;
+	cin>>withdrawValue;
+	if (operations.balance < withdrawValue){
+		cout<<"No enough balance"<<endl;
+	}
+	else {
+		operations.balance -= withdrawValue;
+	}
+	cout<<"Dear customer your new balance is: "<<operations.balance<<endl; 
+	
+}
+
+
+void MyBankActions::displayStatement(){
+	cout<<"Your current balance is: "<<operations.balance<<endl;
+}
+
+class UserDataBase : public PrintingFiles { 
+	public: 
+        
+		string name;
         long int balance = 0;
         string user_account;
         int normal_user_length = 6;
@@ -14,12 +101,22 @@ class UserDataBase {
 
         //UserDataBase(); //constructor;
         //~UserDataBase(); //deconstructor;
+	UserDataBase() {
+        CreateUser();
+    }
+
+    // Destructor
+    ~UserDataBase() {
+	}
 
     void CreateUser();
     private:
 	int value = 0;
 	char* GenerateAccountNumber(int length);
+	void makeMyfile();
 };
+
+
 
 char* UserDataBase::GenerateAccountNumber(int length){
 	const char values[] ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRTUVWXYZ012345679";
@@ -37,7 +134,6 @@ char* UserDataBase::GenerateAccountNumber(int length){
 void UserDataBase::CreateUser(){
 	
 	cout<<"What is your name?"<<endl;
-	string name;
 	getline(cin,name);
 	cout<<"How much would you like to put in your bank account?"<<endl;
 	cin>>balance;
@@ -50,69 +146,23 @@ void UserDataBase::CreateUser(){
 
 }
 
-class MyBankActions : public UserDataBase {
-public:
-	void deposit();
-	void withdraw();
-	void displayStatement();
-	void transfer();
 
 
-};
-
-void MyBankActions::deposit(){
-	int depositValue = 0;
-	
-	cout<<"You chose to deposit!"<<endl;
-	
-	cin>>depositValue;
-	
-	balance += depositValue;
-
-	cout<<"Dear customer your new balance is: "<<balance<<endl;
-}
-
-void MyBankActions::withdraw(){
-	int withdrawValue = 0;
-	
-	cout<<"You chose to withdraw!"<<endl;
-
-	cin>>withdrawValue;
-	if (balance < withdrawValue){
-		cout<<"No enough balance"<<endl;
-	}
-	else {
-		balance -= withdrawValue;
-	}
-	cout<<"Dear customer your new balance is: "<<balance<<endl; 
-	
-}
-
-void MyBankActions::displayStatement(){
-    
-}
-
-void MyBankActions::displayStatement(){
-	cout<<"Your current balance is: "<<balance<<endl;
-}
-
-void transfer(){
-	cout<<"Please enter the user's bank name: "<<endl;
-	
-}
 
 
 int  main(){
-    string name ; 
+    /*
+	string name ; 
     int balance; 
     cout<<"What is the user name for your bank account?"<<endl;
     getline(cin,name);
     cout<<"what are the initial balance you want to put in your account?"<<endl;
     cin>>balance;
-
+	*/
     UserDataBase Maickel;
-    Maickel.balance = balance;
-    Maickel.CreateUser();
+    cout << "User Name: " << Maickel.name << endl;
+    cout << "User Account: " << Maickel.user_account << endl;
+    cout << "User Balance: " << Maickel.balance << endl;
     
     return 0;
 }
